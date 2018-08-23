eee05e8f8b44ac301b56dcc47136453fbab93566
leodiram <sriramscience7@gmail.com>
2018-08-23 12:15:36 +0530

    ham: Drop USB HAL services
    * We don't have dual role usb type-c
    
    Signed-off-by: leodiram <sriramscience7@gmail.com>

--- treble.mk
+++ treble.mk
@@ -93,10 +93,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl        
